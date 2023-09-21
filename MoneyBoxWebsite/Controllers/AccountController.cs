d04d410439d9c20ae1e496f0ef72610ab0539b0e
Victor Mauroy <mauroy.victor@gmail.com>
2023-09-21 16:44:48 +0200

    feat: :sparkles: :construction: replace scaffolded account system by hand-made scripts. Currently working on registering users

---
+++ MoneyBoxWebsite/Controllers/AccountController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using MoneyBoxWebsite.Models;
+using MoneyBoxWebsite.Models.ViewModels;
+using System.Security.Claims;
+
+namespace MoneyBoxWebsite.Controllers
+{
+    public enum Theme
+    {
+        Default,
+        Dark,
+        Blue
+    }
+
+    public class AccountController : Controller
+    {
+        private SignInManager<Client> _signInManager;
+        private UserManager<Client> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(SignInManager<Client> signInManager, UserManager<Client> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Register")]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel registerModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            Client client = new Client
+            {
+                FirstName = registerModel.FirstName,
+                Name = registerModel.LastName,
+                Email = registerModel.Email,
+                Address = registerModel.Address,
+                PhoneNumber = registerModel.PhoneNumber,
+                UserName = registerModel.Username,
+                CurrentTheme = Enum.GetName(typeof(Theme), 0)
+            };
+
+            
+
+
+            /******************         PASSWORD HASHING ?      ******************/
+            IdentityResult result = await _userManager.CreateAsync(client, registerModel.Password);
+            
+            
+            
+            
+            if (result.Succeeded)
+            {
+                await _userManager.AddToRoleAsync(client, "Client"); //Client has now the role CLIENT
+                
+                Microsoft.AspNetCore.Identity.SignInResult resultSignIn = 
+                    await _signInManager.PasswordSignInAsync(
+                        registerModel.Username, 
+                        registerModel.Password, 
+                        registerModel.RememberMe, // Add a cookie ? Right ?
+                        false
+                    );
+
+                if (resultSignIn.Succeeded)
+                {
+                    return RedirectToAction("Index", "Library");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View();
+        }
+
+
+    }
+}
---
+++ MoneyBoxWebsite/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MoneyBoxWebsite.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        public required string Username { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Address { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+        public required string ConfirmedPassword { get; set; }
+        public required string PhoneNumber { get; set; }
+        public bool RememberMe { get; set; } = false;
+    }
+}
