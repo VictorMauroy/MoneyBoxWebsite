20ef719c066da0bf16635830a7e9dde6bddf069c
Victor Mauroy <mauroy.victor@gmail.com>
2023-09-24 18:10:15 +0200

    feat: :sparkles: add a check with a regular expression for the password field in the register form

--- MoneyBoxWebsite/Models/ViewModels/RegisterViewModel.cs
+++ MoneyBoxWebsite/Models/ViewModels/RegisterViewModel.cs
@@ -8,16 +8,20 @@ namespace MoneyBoxWebsite.Models.ViewModels
         public required string Username { get; set; }
         
         [DataType(DataType.Text)]
+        [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "Enter only letters.")]
         public required string FirstName { get; set; }
         
         [DataType(DataType.Text)]
+        [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "Enter only letters.")]
         public required string LastName { get; set; }
         
         [DataType(DataType.EmailAddress)] //[EmailAddress] is an alternative.
         public required string Email { get; set; }
 
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)] //Hide every character entered by the user.
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [RegularExpression(@"^(?=(.*\d))(?=(.*[A-Z]))(?=(.*[a-z]))(?=(.*[^A-Za-z0-9]))(?=.*(.)(.*\1))", 
+            ErrorMessage = "The password must contains at least one number, one lowercase and one uppercase letter and lastly, one non-alphanumeric character.")] 
         public required string Password { get; set; }
 
         [DataType(DataType.Password)]
@@ -27,7 +31,6 @@ namespace MoneyBoxWebsite.Models.ViewModels
         [DataType(DataType.Text)]
         public required string Address { get; set; }
 
-        [DataType(DataType.PhoneNumber)] //It's pointless, doesn't check anything
         public required string PhoneNumber { get; set; }
     }
 }
