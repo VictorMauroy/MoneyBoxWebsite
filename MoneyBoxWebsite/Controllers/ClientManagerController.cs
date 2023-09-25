eba6f437e43e74233f60a8c3c7a150b971d8a191
Victor Mauroy <mauroy.victor@gmail.com>
2023-09-25 16:12:33 +0200

    feat: :construction: ongoing work to show a list of users with their roles

---
+++ MoneyBoxWebsite/Controllers/ClientManagerController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using MoneyBoxWebsite.Models;
+using MoneyBoxWebsite.Repositories;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoneyBoxWebsite.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class ClientManagerController : Controller
+    {
+        private IUserRepository _userRepository;
+        public ClientManagerController(IUserRepository userRepository) 
+        { 
+            _userRepository = userRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Client> clients = await _userRepository.GetAllClients();
+            IEnumerable<Client> orderedClients = from Client client in clients
+                                                  orderby client.Id descending
+                                                  select client; // Show the last created accounts first.
+            return View(orderedClients);
+        }
+    }
+}
