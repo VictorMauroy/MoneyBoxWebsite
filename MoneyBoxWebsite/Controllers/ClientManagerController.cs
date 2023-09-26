f365dca8e6b2edcb37683461b526ea5d610ddd1c
Victor Mauroy <mauroy.victor@gmail.com>
2023-09-26 15:26:25 +0200

    feat: :sparkles: can now update roles for users

--- MoneyBoxWebsite/Controllers/ClientManagerController.cs
+++ MoneyBoxWebsite/Controllers/ClientManagerController.cs
@@ -5,6 +5,12 @@ using MoneyBoxWebsite.Models;
 using MoneyBoxWebsite.Repositories;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Data;
+using Microsoft.Build.Framework;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MoneyBoxWebsite.Controllers
 {
@@ -12,9 +18,16 @@ namespace MoneyBoxWebsite.Controllers
     public class ClientManagerController : Controller
     {
         private IUserRepository _userRepository;
-        public ClientManagerController(IUserRepository userRepository) 
+        private RoleManager<IdentityRole> _roleManager;
+        private UserManager<Client> _userManager;
+        public ClientManagerController(
+            IUserRepository userRepository, 
+            RoleManager<IdentityRole> roleManager, 
+            UserManager<Client> userManager) 
         { 
             _userRepository = userRepository;
+            _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
@@ -31,7 +44,36 @@ namespace MoneyBoxWebsite.Controllers
         {
             Client client = await _userRepository.GetClientById(id);
 
+            var roleNames = _roleManager.Roles.Select(r => r.Name).OrderBy(r => r).ToList();
+            ViewBag.RolesList = roleNames;
+
+            List<string> currentRole = (List<string>) await _userManager.GetRolesAsync(client);
+            ViewBag.CurrentRole = currentRole.Count > 0 ? currentRole[0] : "";
+
             return View(client);
+        }
+
+        /*[HttpPost]
+        public async Task<IActionResult> ChangeActivationState()
+        {
+
+        }*/
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeRole(Guid id)
+        {
+            string newRole = Request.Form["SelectedRole"];
+            string previousRole = Request.Form["PreviousRole"];
+            
+            Client client = await _userRepository.GetClientById(id);
+
+            if(previousRole != "")
+                await _userManager.RemoveFromRoleAsync(client, previousRole);
+            
+            if(await _roleManager.RoleExistsAsync(newRole))
+                await _userManager.AddToRoleAsync(client, newRole);
+
+            return RedirectToAction("Manage", new {id = id});
         }
     }
 }
