94f72ad81a24710ae3fe8b476213e5ad2c94301f
Victor Mauroy <mauroy.victor@gmail.com>
2023-09-27 11:17:55 +0200

    feat: :sparkles: :art: update product management pages and style

---
+++ MoneyBoxWebsite/Data/FileUploader.cs
@@ -0,0 +1,35 @@
+﻿namespace MoneyBoxWebsite.Data
+{
+    public class FileUploader
+    {
+        /// <summary>
+        /// Upload an image file on the server. Upload folder : wwwroot/images.
+        /// </summary>
+        /// <param name="imageFile">The image to upload.</param>
+        /// <returns>The path where the image is saved.</returns>
+        public string UploadImage(IFormFile imageFile)
+        {
+            if(imageFile != null && imageFile.Length > 0)
+            {
+                // Rename the file before inserting into the folder
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+
+                // Specify where to insert the new file
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", fileName);
+
+                Console.WriteLine(" \n \n Image file path" + filePath + "\n \n");
+
+                // Upload file on the server
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    imageFile.CopyTo(stream);
+                }
+
+                return filePath;
+            }
+
+
+            return ""; // In case any error occured
+        }
+    }
+}
